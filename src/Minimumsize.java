5e7608ae7fb3c546080a636ce786d0fde8fb641e
sunil <sunilkumarfsd12@gmail.com>
2026-02-19 11:10:17 +0530

    Leet Code problem

--- src/Minimumsize.java
+++ src/Minimumsize.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Minimumsize{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
 
         System.out.print("Enter the size of array : ");
