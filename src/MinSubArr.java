bbd4a43af60d0ee4c398e1076b367d426a38a538
sunil <sunilkumarfsd12@gmail.com>
2026-02-19 15:51:05 +0530

    Sliding window problem

---
+++ src/MinSubArr.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class MinSubArr{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Enter the size of array : ");
+        int size=scan.nextInt();
+
+        int[] arr=new int[size];
+
+        System.out.println("Enter the array elements : ");
+        for(int i=0;i<size;i++)
+            arr[i]=scan.nextInt();
+
+        System.out.println("Enter the target : ");
+        int target=scan.nextInt();
+
+        System.out.print(minSubArrayLen(arr,target));
+    }
+    static int minSubArrayLen(int[] nums, int target) {
+        int min=Integer.MAX_VALUE;
+        int sum=0;
+        int start=0;
+        for(int end=0;end<nums.length;end++){
+            sum+=nums[end];
+            while(sum>=target){
+                min=Math.min(min,end-start+1);
+                sum-=nums[start];
+                start++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
