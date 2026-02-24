1a1318ec42ee018b8749d48a2f12d150b98cfa9e
sunil <sunilkumarfsd12@gmail.com>
2026-02-24 13:30:16 +0530

    changed variable names

--- src/Sortersquare.java
+++ src/Sortersquare.java
@@ -11,7 +11,15 @@ class Sortersquare {
         for(int i=0;i<nums.length;i++){
             nums[i]=nums[i]*nums[i];
         }
-        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
         return nums;
     }
 }
