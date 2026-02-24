a455f5a85505f56ea5a391f4abf995a5cd907de9
sunil <sunilkumarfsd12@gmail.com>
2026-02-24 14:13:12 +0530

    Optimized method to run in O(n)

--- src/Sortersquare.java
+++ src/Sortersquare.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 class Sortersquare {
     public static void main(String[] args){
-        int[] nums={-4,-2,5,0,3,7,9};
+        int[] nums={-4,-2,-1,0,3,7,9};
         System.out.print(Arrays.toString(sortedSquares(nums)));
     }
     public static int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
+       /* for(int i=0;i<nums.length;i++){
             nums[i]=nums[i]*nums[i];
         }
         for (int i = 0; i < nums.length - 1; i++) {
@@ -20,6 +20,28 @@ class Sortersquare {
                 }
             }
         }
-        return nums;
+        return nums;*/
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index] = nums[right] * nums[right];
+                right--;
+            }
+
+            index--;
+        }
+
+        return result;
     }
 }
