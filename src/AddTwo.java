7da4a14a7ba110a2f62400b46d622fdf8cc6e21c
sunil <sunilkumarfsd12@gmail.com>
2026-02-26 18:25:29 +0530

    Add to Integers

---
+++ src/AddTwo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AddTwo {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int num1=scan.nextInt();
+        System.out.println("Enter the second number : ");
+        int num2=scan.nextInt();
+        System.out.println(num1+num2);
+    }
+}
