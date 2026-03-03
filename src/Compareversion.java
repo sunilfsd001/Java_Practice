8c482056001701a4065059d8ed0b4850768f1a04
sunil <sunilkumarfsd12@gmail.com>
2026-03-03 11:56:58 +0530

    Added another method to solve the problem efficiently without adding zero's in the end and using list and splitting

--- src/Compareversion.java
+++ src/Compareversion.java
@@ -1,12 +1,39 @@
 import java.util.*;
-public class Compareversion{
+class Compareversion{
     public static void main(String[] args) {
-        String v1="1.01";
-        String v2="1.0001";
-        System.out.println(compareVersion(v1,v2));
+        String v1="1.2";
+        String v2="1.00010";
+        System.out.println(compareVersion1(v1,v2));
+        System.out.println(compareVersion2(v1,v2));
     }
 
-    static public int compareVersion(String version1, String version2) {
+    static int compareVersion2(String version1, String version2) {
+        int v1Size=version1.length();
+        int v2Size=version2.length();
+
+        int i=0,j=0;
+
+        while(i<v1Size || j<v2Size){
+            int v1=0,v2=0;
+            while(i<v1Size && version1.charAt(i)!='.'){
+                v1=v1*10+version1.charAt(i)-'0';
+                i++;
+            }
+            while(j<v2Size && version2.charAt(j)!='.'){
+                v2=v2*10+version2.charAt(j)-'0';
+                j++;
+            }
+            if(v1<v2) return -1;
+            else if(v1>v2) return 1;
+
+            i++;
+            j++;
+        }
+        return 0;
+    }
+
+
+    static int compareVersion1(String version1, String version2) {
         List<Integer> v1 = splitNum(version1);
         List<Integer> v2 = splitNum(version2);
         if (v1.size() < v2.size()) {
