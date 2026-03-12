563fa94a9a4e4de093a7319a5ae4cdad9cb0885e
sunil <sunilkumarfsd12@gmail.com>
2026-03-12 16:55:17 +0530

    Counting asc

--- src/Factorial.java
+++ src/Factorial.java
@@ -7,7 +7,7 @@ public class Factorial {
         System.out.print("Factorial of "+n+" Numbers is : "+factorial(n));
     }
     static int factorial(int n){
-        if(n==1)return 1;
+        if(n<=1)return 1;
         return n*factorial(n-1);
     }
 }
