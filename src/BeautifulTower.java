fca103faf1cacffc7c97ed33a3bb923a76b4bbc3
Sunil Kumar T <sunilkumarfsd12@gmail.com>
2026-03-25 14:53:13 +0530

    Adding Leetcode problems

---
+++ src/BeautifulTower.java
@@ -0,0 +1,56 @@
+class BeautifulTower{
+    public static void main(String[] args){
+        int[] heights={5,3,4,1,1};
+        System.out.println(maximumSumOfHeights(heights));
+    }
+    public long maximumSumOfHeights(int[] maxHeights) {
+
+
+            int n = maxHeights.length;
+        long maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            long sum = maxHeights[i];
+            int leftHeight = maxHeights[i];
+            int rightHeight = maxHeights[i];
+
+            for (int j = i - 1; j >= 0; j--) {
+                leftHeight = Math.min(leftHeight, maxHeights[j]);
+                sum += leftHeight;
+            }
+
+            for (int k = i + 1; k < n; k++) {
+                rightHeight = Math.min(rightHeight, maxHeights[k]);
+                sum += rightHeight;
+            }
+
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+
+
+    //     int max=heights[0];
+    //     for(int i=1;i<heights.length;i++){
+    //         if(max<heights[i]){
+    //             max=heights[i];
+    //         }
+    //     }
+    //     long result=0;
+    //     for(int num=0;num<heights.length;num++){
+    //         if(heights[num]==max){
+    //             for(int i=num-1;i>=0;i--){
+    //                 heights[i]=Math.min(heights[i],heights[i+1]);
+    //             }
+    //             for(int i=num+1;i<heights.length;i++){
+    //                 heights[i]=Math.min(heights[i-1],heights[i]);
+    //             }
+    //             long curResult=0;
+    //             for(int n:heights){
+    //                 curResult+=n;
+    //             }
+    //             result=Math.max(result,curResult);
+    //         }
+    // }
+    //     return result;
+    }
+}
---
+++ src/Kthlargest.java
@@ -0,0 +1,38 @@
+class Kthlargest{
+    public static void main(String[] args){
+        int[] nums={3,2,1,5,6,4};
+        int k=2;
+        System.out.println(findKthLargest(nums,k));
+    }
+    static int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue <Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+        for(int n: nums){
+            maxHeap.add(n);
+        }
+        for(int count=1;count<k;count++){
+            maxHeap.poll();
+        }
+        return maxHeap.peek();
+
+    // while(k > 0){
+
+    //     int max = Integer.MIN_VALUE;
+    //     int idx = -1;
+
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i] > max){
+    //             max = nums[i];
+    //             idx = i;
+    //         }
+    //     }
+
+    //     nums[idx] = Integer.MIN_VALUE;
+    //     k--;
+
+    //     if(k == 0) return max;
+    // }
+
+    // return -1;
+    }
+}
